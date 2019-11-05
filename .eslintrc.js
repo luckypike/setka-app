4038aaf9c163f71fe36eba3f711a4b4600998cb6
Andy Braga <bragovo@gmail.com>
2019-11-06 00:45:21 +0300

    Linter и пустой компонент

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
-};
+  extends: [
+    '@react-native-community',
+    'standard'
+  ],
+  rules: {
+    'prettier/prettier': 0,
+  }
+}
