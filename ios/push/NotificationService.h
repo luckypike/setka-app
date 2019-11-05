6ec02f44bb8e672aae42f98b8984f8c7220c420a
Andy Braga <bragovo@gmail.com>
2019-11-06 00:55:40 +0300

    Добавлены профили и нотифы

---
+++ ios/push/NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  push
+//
+//  Created by Andy Braga on 06.11.2019.
+//  Copyright © 2019 Setka. All rights reserved.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
